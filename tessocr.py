0c192d860a87238ea59dcf99ab0eaa1c4a925ee0
louie.zl <1214279441@qq.com>
2018-10-14 21:45:15 +0800

    Update tessocr.py

--- tessocr.py
+++ tessocr.py
@@ -22,15 +22,20 @@ def binarization(image):
 
 def get_GPS_info(image,className,showPic=False,offsetx1=0,offsetx2=0,offsety1=0,offsety2=0):
     # image = Image.open(imageFile)
+    # 已经通过实验找到图片中的GPS固定位置
     regionx=(882,1000,1117,1058)#x的坐标
     regiony=(1130,1000,1342,1058)#x的坐标
     out = binarization(image)
     out_y=out.crop(regiony)
     out_x=out.crop(regionx)
+    # 识别经度字符串
     text_x = pytesseract.image_to_string(out_x,config='-l eng')  # 0使用eng文解析图片
+    # 过滤无效信息
     num_x = re.sub("\D", "", text_x)
     num_x = num_x[:3] + '.' + num_x[3:]
+    # 识别纬度字符串
     text_y = pytesseract.image_to_string(out_y,config='-l eng')  # 0使用eng文解析图片
+    # 过滤无效信息
     num_y = re.sub("\D", "", text_y)
     num_y = num_y[:2] + '.' + num_y[2:]
     if showPic:
